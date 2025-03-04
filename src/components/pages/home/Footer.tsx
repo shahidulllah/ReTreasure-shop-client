5ffd2e9cd1fd2c9150407f7bde0fe3ec8ef19195
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-04 20:14:19 +0600

    simple footer added

---
+++ src/components/pages/home/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-900 text-white py-4 text-center">
+        <p>© {new Date().getFullYear()} SecondHand Marketplace. All rights reserved.</p>
+      </footer>
+    );
+  }
+  

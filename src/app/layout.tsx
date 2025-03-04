d4ed521a985407e635bb9d7c8c094a2fec065c49
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-04 20:27:16 +0600

    auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Navbar from "@/components/pages/home/Navbar";
+import Footer from "@/components/pages/home/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster position="top-center" richColors/>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );

debba78e55e4983e380ee26ceeb9eef7456b8350
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-18 03:27:35 +0600

    update profile api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -5,7 +6,21 @@ const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const fetchSellerDetails = async (sellerId: string) => {
   try {
     const response = await axios.get(`${API_URL}/api/users/${sellerId}`);
-    
+
+    return response.data.user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateProfile = async ({ userId, data }: any) => {
+  try {
+    const response = await axios.put(
+      `${API_URL}/api/users/profile/${userId}`,
+      data
+    );
+
     return response.data.user;
   } catch (error) {
     console.log(error);

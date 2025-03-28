e08f7e947cc4a3294df57cfed9558829591ab753
shahidulllah <mdshahidsumon177@gmail.com>
2025-03-28 14:55:50 +0600

    wishlist func added

--- src/components/shared/ListingCard.tsx
+++ src/components/shared/ListingCard.tsx
@@ -1,8 +1,15 @@
+import {
+  addToWishlist,
+  removeFromWishlist,
+  selectWishlist,
+} from "@/redux/features/wishListSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { IListing } from "@/types";
 import { getTimeAgo } from "@/utils/getTime";
 import { Bookmark, Layers, MapPin } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ListingCardProps {
   listing: IListing;
@@ -10,6 +17,20 @@ interface ListingCardProps {
 
 const ListingCard = ({ listing }: ListingCardProps) => {
   const router = useRouter();
+  const dispatch = useAppDispatch();
+  const wishlist = useAppSelector(selectWishlist);
+
+  //check wish item is already added
+  const isInWishlist = wishlist.some((item) => item._id === listing._id);
+
+  const handleWishlistToggle = () => {
+    if (isInWishlist) {
+      dispatch(removeFromWishlist(listing._id));
+    } else {
+      dispatch(addToWishlist(listing));
+      toast.success('Saved successfully..!')
+    }
+  };
   return (
     <div className=" border border-purple-300 rounded-lg overflow-hidden hover:shadow-lg transition-shadow space-y-3 max-h-[370px]">
       <div
@@ -35,7 +56,12 @@ const ListingCard = ({ listing }: ListingCardProps) => {
           <p className="text-xs text-gray-400 mt-2">
             {getTimeAgo(listing.createdAt)}
           </p>
-          <Bookmark className="cursor-pointer" />
+          <Bookmark
+            className={`cursor-pointer transition ${
+              isInWishlist ? "text-purple-700 fill-purple-700" : "text-gray-500"
+            }`}
+            onClick={handleWishlistToggle}
+          />
         </div>
       </div>
     </div>

59c5da3cc00273b01548b848106ebfa2a0e4df16
shahidulllah <mdshahidsumon177@gmail.com>
2025-12-05 19:05:11 +0600

    image uploader component

---
+++ src/components/shared/ImageUploader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function ImageUploader() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/upload`, {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    setImageUrl(data.url);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleUpload} />
+
+      {imageUrl && (
+        <Image src={imageUrl} alt="uploaded" className="mt-4 w-40 h-auto" />
+      )}
+    </div>
+  );
+}
